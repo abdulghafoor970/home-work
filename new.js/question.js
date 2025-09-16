84058a48f7d476bf553a7544afcec8e6c7759681
abdulghafoor970 <imabdulghafoor9@gmail.com>
2025-09-16 14:24:30 +0500

    half work done

--- new.js/question.js
+++ new.js/question.js
@@ -2,24 +2,108 @@
 
 // // 1. Ask the user for their age. If the age is 18 or more, show an alert: "You are an
 // // adult.", else show: "You are a minor."
+//    var age =prompt ( "how old are you?");
+//    if(age >= 18 ){
+//     alert("you are adult.");
+
+//    }else{
+//     alert("you are minor.");
+
+//    }
 // // 2. Ask the user to enter a number. If it's positive, log: "Positive number"; else log:
 // // "Not positive".
+    // var number = prompt ("write down any number.");
+    // if(number > 0){
+    //     console.log("it is a positive number.");
+        
+    // }else {
+    //     console.log("it is negative number .");
+
+        
+    // }
 // // 3. Ask the user for a number. If it’s even, alert: "Even number"; else alert: "Odd
 // // number".
+    //   var number =prompt("write down any number. ");
+    //   if(number % 2 === 0 ){
+    //     alert("it is an even number.");
+
+    //   }else{
+    //     alert("it is an odd number.");
+    //   }
 // // 4. Ask for a username. If the username is "admin", display "Welcome, admin!"; else
 // // display "Access denied."
+    //   var username = prompt ( "write down name .");
+    //   if(username == "admin" || username == "Admin"){
+    //     alert("wellcome, admin!");
+    //   }else{
+    //     alert("get lost from here");
+    //   }
 // // 5. Ask for two numbers. If they are equal, log "Both numbers are the same";
 // // otherwise, log "The numbers are different".
+    //    var num1 = prompt("write down first number.");
+    //    var num2 = prompt("write down second number.");
+    //    if (num1 === num2  ){
+    //     console.log("Both numbers are the same");
+    //    }else{
+    //     console.log("the numbers are different.");
+        
+    //    }
 // // 6. Ask the user to enter two numbers. Check which one is larger and alert: "First is
 // // larger" or "Second is larger".
+    //   var num1 = prompt("write down first number");
+    //   var num2 = prompt("write down second number");
+    //   if(isNaN(num1) || isNaN(num2)){
+    //     alert("write a valid number.");
+
+    //   }else if(num1 > num2 ){
+    //     alert("first number is greater than second number.");
+    //   }else {
+    //     alert("second number is greater than first number. ");
+    //   }
 // // 7. Ask for a number. If it's greater than 100, alert "Too big!", else alert "Within
 // // range".
+    //    var number = prompt("write down any number.")
+    //    if (number > 100){
+    //     alert("too big.");
+
+    //    }else{
+    //     alert("Within range .");
+
+    //    }
 // // 8. Ask for a score. If it’s 50 or higher, log "You passed!", else log "You failed."
+//   var score = prompt("what was your score?");
+//   if (score > 50 ){
+//     alert("you have passed~!");
+//   }else{
+//     alert("you have not passed !");
+//   }
 // // 9. Ask the user to enter a temperature. If it’s over 30, alert "It's hot!"; else alert "It's
 // // not hot."
+    //   var temp =prompt("what is the temperature ?");
+    //   if (temp >= 30 ){
+    //     alert("it is too hot!");
+    //   }else{
+    //     alert("it is not hot !");
+    //   }
 // // 10. Ask the user to enter a number. If it's 0, alert "Zero"; if it’s not zero, alert "Not zero".
+    //   var number =prompt("write down any number ");
+    //   if (number > 0 || number < 0){
+    //     alert("not zero!");
+
+    //   }else{
+    //     alert("it is zero!!");
+
+    //   }
 // // 11. Ask the user for their age. If it's 60 or more, log "You are a senior citizen.",
 // // else log "You are not a senior."
+    //   var age = prompt("what is your age ?");
+    //   if (age >= 60 ){
+    //     console.log(" you are a senior citizen!");
+    //   }else {
+    //     console.log("you are not senior");
+
+        
+    //   }
 // // 12. Ask the user for their name. If the name is "Alice", alert "Hi, Alice!"; else alert
 // // "You're not Alice!"
 
@@ -48,6 +132,7 @@
 // // Write JavaScript if - else if - else conditions to solve the following problems. Use
 // // logical operators like && and || where applicable. Do not write solutions here—just practice
 // // writing the condition blocks.
+        
 
 // // 1. Age-Based Grade Check
 // // Check a student's marks and display:
@@ -56,12 +141,34 @@
 // // ● "Fail" if marks < 50
 // // ● "Invalid Marks" if marks are not between 0 and 100
 
+        //  var score =prompt(" what is your score ?");
+        //  if (score >= 80 ){
+        //     alert("distinction!!");
+
+        //  }else if (score >= 50 && score <80 ){
+        //     alert(" pass!! ");
+
+        //  }else{
+        //     alert(" failed");
+        //  }
+
 // // 2. Discount Eligibility Based on Age and Membership
 // // Given a person's age and membership status, show:
 // // ● "Senior Premium Discount" if age ≥ 60 and is a premium member
 // // ● "Senior Discount" if age ≥ 60
 // // ● "Premium Discount" if a premium member
 // // ● "No Discount" otherwise
+     var age = prompt ("what is your age ?");
+     var statu = prompt (" what is your status?");
+     if (age >= 60 && statu === "premium member"){
+        alert("Senior Premium Discount");
+     }else if(age >= 60 && statu === "none" ){
+        alert("Senior Discount! ");
+     }else if(age >= 30 && age < 60 && statu === "premium member"){
+        alert("Premium Discount");
+     }else{
+        alert("No Discount");
+     }
 
 // // 3. Login and Email Verification
 // // ● If user is logged in and email is verified → "Access granted"
